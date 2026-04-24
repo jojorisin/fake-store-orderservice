893d0b1b1d6609a8ce6ea3f4887218dc108d514d
johannajonsson <jojo.risin@outlook.com>
2026-04-24 12:04:15 +0200

    first commit

---
+++ src/main/java/se/jensen/johanna/fakestoreorderservice/FakeStoreOrderserviceApplication.java
@@ -0,0 +1,13 @@
+package se.jensen.johanna.fakestoreorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeStoreOrderserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FakeStoreOrderserviceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/se/jensen/johanna/fakestoreorderservice/FakeStoreOrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensen.johanna.fakestoreorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreOrderserviceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
